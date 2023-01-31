a9b99b2d47b671266b04acbd3d4fcabae87ba578
Melvyn Drag <melvyniandrag@gmail.com>
2023-01-31 06:14:05 -0500

    more work

--- Strict.sql
+++ Strict.sql
@@ -1,3 +1,4 @@
+-- note this requires sqlite 3.37 or higher
 create table rules(
 	a INT,
 	b INTEGER,
@@ -5,7 +6,7 @@ create table rules(
 	d TEXT,
 	e BLOB, 
 	f ANY
-) ;
+) Strict ;
 
 insert into rules values(1, 1, 1.0, "hello", "hello", "hello");
 insert into rules values("hello", 1, 1.0, "hello", "hello", "hello");
